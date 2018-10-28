1ab64fa0f4fff06085a7706d03d6e091a8b28c62
Victor Ukafor <victorukafor@gmal.com>
2018-10-28 23:25:00 +0100

    chore(setup): configures eslint using Airbnb
    - configures linting with eslint using Airbnb standards
    
    [Finishes #160618053]

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Welcome to Authors&apos; Haven</p>
+    </header>
+  </div>
+);
+
+export default App;
