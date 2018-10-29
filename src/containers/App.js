c9ef31f7b3ed27c0080a5660d2597ac7e9f17e7e
Anthony Ugwu <anthonyugwu234@gmail.com>
2018-10-29 18:20:42 +0100

    install dependencies

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import '../styles/App.css';
 
-
 const App = () => (
-  <div className="App">
-    <header>
-      <p>Welcome to Authors&apos; Haven</p>
-    </header>
-  </div>
+  <Provider>
+    <div className="App">
+      <header>
+        <p>Welcome to Authors&apos; Haven</p>
+      </header>
+    </div>
+  </Provider>
 );
 
 export default App;
