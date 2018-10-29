ff3fcf81d4f80d4319a21814417847ec1aea65c9
Anthony Ugwu <anthonyugwu234@gmail.com>
2018-10-29 18:27:28 +0100

    create store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import '../styles/App.css';
+import store from '../store';
 
 const App = () => (
-  <Provider>
+  <Provider store={store}>
     <div className="App">
       <header>
         <p>Welcome to Authors&apos; Haven</p>
