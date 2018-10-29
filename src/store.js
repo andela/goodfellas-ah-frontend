1389a7e7d1fe75278301ae3dd756e63ef38dbec3
Anthony Ugwu <anthonyugwu234@gmail.com>
2018-10-29 18:27:28 +0100

    create store

--- src/store.js
+++ src/store.js
@@ -3,8 +3,15 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+<<<<<<< HEAD
 const initialState = {};
 const enhancers = composeWithDevTools({});
 const store = createStore(rootReducer, initialState, enhancers(applyMiddleware(thunk)));
+=======
+const middleware = [thunk];
+const initialState = {};
+const enhancers = composeWithDevTools({});
+const store = createStore(rootReducer, initialState, enhancers(applyMiddleware(...middleware)));
+>>>>>>> create store
 
 export default store;
