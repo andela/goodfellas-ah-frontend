8d945ffca179cdf30a174355168a42e5f03270a1
Anthony Ugwu <anthonyugwu234@gmail.com>
2018-10-29 18:20:42 +0100

    install dependencies

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import HomePage from '../components/HomePage';
-import Login from '../components/Login';
 import '../styles/styles.scss';
+import store from '../store';
+import Sample from '../components/Sample';
 
-export default () => (
-  <div className="App">
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Login">Login</Link>
-        </li>
-      </ul>
-    </nav>
-    <Route exact path="/" component={HomePage} />
-    <Route path="/Login" component={Login} />
-  </div>
-
+const App = () => (
+  <Provider store={store}>
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/Login" component={Sample} />
+    </div>
+  </Provider>
 );
+
+export default App;
