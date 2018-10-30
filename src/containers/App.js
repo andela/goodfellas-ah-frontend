c68d2bc518d43f1f6d8dd4b129411c2a8faa15c6
ali <alishaibu2002@gmail.com>
2018-10-30 11:54:27 +0100

    fix(Setup-react-router) Setup react router version 4

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import HomePage from '../components/HomePage';
 import Login from '../components/Login';
 import '../styles/styles.scss';
 
 export default () => (
-  <BrowserRouter>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
-        </ul>
-      </nav>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/Login" component={Login} />
-    </div>
-  </BrowserRouter>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+    <Route exact path="/" component={HomePage} />
+    <Route path="/Login" component={Login} />
+  </div>
+
 );
