55a2779788181cf6a9786b43686c227190a23297
ali <alishaibu2002@gmail.com>
2018-10-30 11:19:29 +0100

    chore(Setup-react-router) Setup react router version 4
    [Finishes #160618057
    ]

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import '../styles/App.css';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import HomePage from '../components/HomePage';
+import Login from '../components/Login';
 
-
-const App = () => (
-  <div className="App">
-    <header>
-      <p>Welcome to Authors&apos; Haven</p>
-    </header>
-  </div>
+export default () => (
+  <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/Login" component={Login} />
+    </div>
+  </BrowserRouter>
 );
-
-export default App;
