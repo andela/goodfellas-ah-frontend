10c0d2297d2f38a949abb814cd0c118fb9ea983a
Anthony Ugwu <anthonyugwu234@gmail.com>
2018-10-30 15:50:17 +0100

    implement an example

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import HomePage from '../components/HomePage';
+import HomePage from '../components/layout/hero';
 import '../styles/styles.scss';
 import store from '../store';
 import Sample from '../components/Sample';
@@ -9,16 +9,6 @@ import Sample from '../components/Sample';
 const App = () => (
   <Provider store={store}>
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
-        </ul>
-      </nav>
       <Route exact path="/" component={HomePage} />
       <Route path="/Login" component={Sample} />
     </div>
