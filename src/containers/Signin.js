96e1f2a504e8d277379126fa2955aecb39d23f4d
AdinoyiSadiq <sadiqadinoyi@gmail.com>
2018-10-31 13:47:31 +0100

    feat(test): add initial failing test for Signin container
    [Delivers #160618062]

---
+++ src/containers/Signin.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Signin = () => <div>This is the Sign-in container</div>;
+
+export default Signin;
---
+++ src/tests/containers/Signin.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Signin from '../../containers/Signin';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<Signin />);
+});
+
+afterEach(() => wrapped.unmount());
+
+it('has two input fields and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(2);
+  expect(wrapped.find('button').length).toEqual(1);
+});
