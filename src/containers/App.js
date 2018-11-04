72c861e0c07a54b0b17e957e8896e23d4326dba7
Anthony Ugwu <anthonyugwu234@gmail.com>
2018-11-04 06:47:49 +0100

    merge signin branch

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import store from '../store';
 import ForgotPassword from '../views/ForgotPassword';
 import ResetPassword from '../views/ResetPassword';
-=======
-import { BrowserRouter, Route } from 'react-router-dom';
 import Auth from '../views/Auth';
 import Hero from '../views/Hero';
 import Profile from '../views/Profile';
 import Header from '../components/shared/Header';
 import authenticate from './hoc/authenticate';
->>>>>>> 0a956bf1c0b1b4693e671da154069b87112abf9e
 import '../styles/styles.scss';
 
 const User = () => (
@@ -27,6 +21,8 @@ const App = () => (
     <div>
       <Route exact path="/" component={Hero} />
       <Route path="/auth" component={Auth} />
+      <Route path="/forgotpassword" component={ForgotPassword} />
+      <Route path="/resetpassword" component={ResetPassword} />
       <Route path="/user" component={authenticate(User)} />
     </div>
   </BrowserRouter>
