56b7f87f679c85a86fe82d642a4b8bce567c8b97
Anthony Ugwu <anthonyugwu234@gmail.com>
2018-11-08 10:20:32 +0100

    reuse modular components

--- src/components/shared/InputField.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const InputField = (props) => (
-  <div className="form-group">
-    <input type={props.type} className="form-control form-control-lg" placeholder={props.placeholder} name={props.name} />
-  </div>
-);
-
-export default InputField;
--- src/components/shared/Submitbtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Submitbtn = () => (
-  <div>
-    <input type="submit" className="btn btn-block mt-4 resetbtn" />
-  </div>
-);
-
-export default Submitbtn;
