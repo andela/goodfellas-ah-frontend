1c035e3a83ed64597b4e8fa0b90e660bf16e3193
AdinoyiSadiq <sadiqadinoyi@gmail.com>
2018-11-09 16:34:45 +0100

    feat(test): add tests for the Social auth page, components and containers
    [Delivers #160618063]

---
+++ src/tests/containers/SocialAuth.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Root from '../../root';
+import SocialAuth from '../../containers/SocialAuth';
+
+let wrapped;
+const history = {
+  location: {
+    search: {
+      token: 'token',
+      userId: 'userId',
+    },
+  },
+};
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <SocialAuth history={history} />
+    </Root>,
+  );
+});
+
+afterEach(() => wrapped.unmount());
+
+describe('SocialAuth UI', () => {
+  describe('render features', () => {
+    test('container should render as expected', () => {
+      const tree = toJson(wrapped);
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
--- src/views/SocialAuthPage.js
+++ src/views/SocialAuthPage.js
@@ -1,31 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import queryString from 'query-string';
-import { socialSignin } from '../actions/authActions';
-import LargeLoader from '../components/shared/LargeLoader';
+import React from 'react';
+import SocialAuth from '../containers/SocialAuth';
+
+const SocialAuthPage = (props) => (
+  <div>
+    <SocialAuth {...props} />
+  </div>
+);
 
 
-class SocialAuthPage extends Component {
-  componentWillMount() {
-    const { socialSignin: socialSigninUser, history } = this.props;
-    const { token, userId } = queryString.parse(history.location.search);
-
-    socialSigninUser({ token, userId }, (response) => {
-      if (response) {
-        history.push('/user/profile');
-      } else {
-        history.push('/auth/signin');
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <LargeLoader />
-      </div>
-    );
-  }
-}
-
-export default connect(null, { socialSignin })(SocialAuthPage);
+export default SocialAuthPage;
