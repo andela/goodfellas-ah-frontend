9ecd6e72b5bcaff374955001259ef59ff6df6f1c
damafeez <damafeez@gmail.com>
2018-11-13 02:21:16 +0100

    fix(test): fix AuthFormToggle.test.js import error

--- src/tests/components/Auth/AuthFormToggle.test.js
+++ src/tests/components/Auth/AuthFormToggle.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import AuthFormToggle from '../../../components/auth/AuthFormToggle';
+import AuthFormToggle from '../../../components/Auth/AuthFormToggle';
 
 const setup = () => {
   const props = {
