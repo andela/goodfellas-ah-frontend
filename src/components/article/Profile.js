187de725e3bca4e458dd519bcbad6213bc25d5bd
damafeez <damafeez@gmail.com>
2018-11-22 18:44:09 +0100

    fix(profile): profile articlecard navigate to read pag
    - enable navigation to read page when article body is clicked
    - add tests to improve coverage
    
    finishes #162153865

--- src/components/article/Profile.js
+++ src/components/article/Profile.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import moment from 'moment';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import icons from '../../assets/icons.svg';
 import { userPlaceholderImage, articlePlaceholderImage, filterReactions } from '../../mixin';
 
-export default (props) => {
+export const Profile = (props) => {
   const {
     article,
     author,
     authorImage,
     userId,
+    history,
   } = props;
   return (
-    <div className="profile-article hoverable">
-      <Link className="profile-article_user-img" to={`/user/profile/${article.authorId}`}><img src={authorImage || userPlaceholderImage} alt="user" /></Link>
+    <div onClick={() => history.push(`/articles/${article.slug}`)} className="profile-article hoverable">
+      <Link onClick={(e) => e.stopPropagation()} className="profile-article_user-img" to={`/user/profile/${article.authorId}`}><img src={authorImage || userPlaceholderImage} alt="user" /></Link>
       <div className="profile-article_img" style={{ backgroundImage: `url(${article.image || articlePlaceholderImage})` }} />
       <h3>{ article.title }</h3>
-      <Link to={`/user/profile/${article.authorId}`} className="profile-article_username p">{author}</Link>
+      <Link onClick={(e) => e.stopPropagation()} to={`/user/profile/${article.authorId}`} className="profile-article_username p">{author}</Link>
       <p className="profile-article_date">{moment(article.createdAt).format('ll')}</p>
       <p className="profile-article_read-time">{`${article.read_time} read`}</p>
       <p className="profile-article_description">{article.description}</p>
@@ -34,8 +35,10 @@ export default (props) => {
           </svg>
         </p>
         <p className="comments">{`${article.comments.length || 0} comments`}</p>
-        {userId === article.authorId && <Link to={`/articles/edit/${article.slug}`} className="edit p">Edit Article</Link>}
+        {userId === article.authorId && <Link onClick={(e) => e.stopPropagation()} to={`/articles/edit/${article.slug}`} className="edit p hoverable">Edit Article</Link>}
       </div>
     </div>
   );
 };
+
+export default withRouter(Profile);
