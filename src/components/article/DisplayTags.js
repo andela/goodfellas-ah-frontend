9a351f66c079d9b50406f080dd06d3da284b3b8f
phunmbi <phunmbi@gmail.com>
2018-11-23 10:37:54 +0100

    refactor(landing page): replace lorem ipsum text

--- src/components/article/DisplayTags.js
+++ src/components/article/DisplayTags.js
@@ -3,13 +3,26 @@ import React from 'react';
 export const DisplayTags = (props) => {
   const { articles } = props;
   let keyTrack = 0;
+
+  const tagCheck = () => {
+    let displayTagsResult;
+    if (articles.tagList === null) {
+      displayTagsResult = null;
+    } else {
+      displayTagsResult = (
+        <div className="tags-display">
+          {articles.tagList.map((tag) => {
+            keyTrack += 1;
+            return <p key={keyTrack} className="new-tag">{tag}</p>;
+          })}
+        </div>
+      );
+    }
+    return displayTagsResult;
+  };
+
   return (
-    <div className="tags-display">
-      {articles.tagList.map((tag) => {
-        keyTrack += 1;
-        return <p key={keyTrack} className="new-tag">{tag}</p>;
-      })}
-    </div>
+    tagCheck()
   );
 };
 
